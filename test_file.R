ee6f0f502cfb02758caff8c297a4e5935ccb1c3d
unknown <nhanthng@yahoo.com>
2021-09-30 16:02:43 -0700

    test list

---
+++ test_file.R
@@ -0,0 +1,25 @@
+(Number<-c(2,33,68))
+Number[c(2)]
+Number[c(3,2,1)]
+(Number<- c(Number, 22))
+Number[3]
+(String<- c("dog","cat","salmon","wolf"))
+class(String)
+
+
+##create dataframe (excel)
+
+data.frame(first_column=String, second_column=String)
+test_list = list('first string entry into list')
+test
+
+test_list[[1]][2] <- 'add next thing'
+test.df = data.frame(1:26,letters)
+head(test.df)
+test_list[[3]]<-test.df
+
+
+test_list = list('first string entry into list')
+test_list[[2]]<-("2nd thing")
+str(test_list)
+
